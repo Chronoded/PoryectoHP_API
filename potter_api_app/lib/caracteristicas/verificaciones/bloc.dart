05634ceda230447ef53c2b32600fe8e39d14e5e3
Chronoded <oli_olli@hotmail.com>
2022-11-26 16:22:24 -0700

    Estados y Eventos Creado, agregue dependencia Bloc

---
+++ potter_api_app/lib/caracteristicas/verificaciones/bloc.dart
@@ -0,0 +1,12 @@
+import 'package:bloc/bloc.dart';
+
+class Estado {}
+
+class Creandose extends Estado {}
+
+class PantallaInicial extends Estado {}
+
+class Evento {}
+
+class Creado extends Evento {}
+
