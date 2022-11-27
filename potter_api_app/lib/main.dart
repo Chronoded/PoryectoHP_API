b2c10922251141d9aae64d135180d3503b9e953d
Chronoded <oli_olli@hotmail.com>
2022-11-26 17:05:12 -0700

    Creacion vista creandose, aplicacion inyectada y bloc provider en main

--- potter_api_app/lib/main.dart
+++ potter_api_app/lib/main.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:potter_api_app/caracteristicas/verificaciones/bloc.dart';
+import 'package:potter_api_app/caracteristicas/verificaciones/vistas/vista_creandose.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const AplicacionInyectada());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AplicacionInyectada extends StatelessWidget {
+  const AplicacionInyectada({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return BlocProvider(
+      create: (context) => BlocVerificacion(),
+      child: const Aplicacion(),
+    );
   }
      
   }
+   class Aplicacion extends StatelessWidget {
+  const Aplicacion({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Builder(builder: (context) {
+          var estado = context.watch<BlocVerificacion>().state;
+          if (estado is Creandose) {
+            return const VistaEnCreacion();
+          }
+          return const Center(child: Text('ERROR'));
+        }),
+      ),
+    );
+  }
+}
