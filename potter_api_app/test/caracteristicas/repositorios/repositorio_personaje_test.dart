bc5eef146f07c58fb273c78662bf13e985d61b97
Chronoded <oli_olli@hotmail.com>
2022-11-30 18:57:55 -0700

    Cree test online de repo y arregle los offline

--- potter_api_app/test/caracteristicas/repositorios/repositorio_personaje_test.dart
+++ potter_api_app/test/caracteristicas/repositorios/repositorio_personaje_test.dart
@@ -4,11 +4,11 @@ import 'package:potter_api_app/caracteristicas/dominio/problema.dart';
 import 'package:potter_api_app/caracteristicas/repositorios/repositorio_personaje.dart';
 
 void main() {
-  group('pruebas de personajes: ', () {
-    test('con Harry Potter me devuelve un personaje', () {
+  group('pruebas de personajes offline: ', () {
+    test('con Harry Potter me devuelve un personaje', () async {
       RepositorioObtenerPersonaje repo = RepositorioObtenerPersonaje();
       var resultado =
-          repo.obtenerPersonaje(NombrePersonaje.contructor('Harry Potter'));
+        await repo.obtenerPersonaje(NombrePersonaje.contructor('Harry Potter'));
       resultado.match((l) {
         expect(false, equals(true));
       }, (r) {
@@ -20,10 +20,10 @@ void main() {
   });
 
    group('pruebas manejo de errores: ', () {
-    test('con Harry Porter arroja error', () {
+    test('con Harry Porter arroja error', () async {
       RepositorioObtenerPersonaje repo = RepositorioObtenerPersonaje();
       var resultado =
-          repo.obtenerPersonaje(NombrePersonaje.contructor('Harry Porter'));
+         await repo.obtenerPersonaje(NombrePersonaje.contructor('Harry Porter'));
       resultado.match((l) {
         expect(l, isA<PersonajeNoEncontrado>());
       }, (r) {
@@ -32,4 +32,30 @@ void main() {
     });
   });
 
+   group('pruebas de personajes online: ', () {
+    test('con Hermione Granger me devuelve un personaje', () async {
+      RepositorioPersonajesReales repo = RepositorioPersonajesReales();
+      var resultado = await repo
+          .obtenerPersonaje(NombrePersonaje.contructor('Hermione Granger'));
+      resultado.match((l) {
+        expect(false, equals(true));
+      }, (r) {
+        expect(r.actor, 'Emma Watson');
+        expect(r.nombre, 'Hermione Granger');
+        expect(r.patronus, 'otter');
+      });
+    });
+
+    test('con Hermione Gringa arroja personaje no encontrado', () async {
+      RepositorioPersonajesReales rp = RepositorioPersonajesReales();
+      var resultado = await rp
+          .obtenerPersonaje(NombrePersonaje.contructor('Hermione Gringa'));
+      resultado.match((l) {
+        expect(l, isA<PersonajeNoEncontrado>());
+      }, (r) {
+        assert(false);
+      });
+    });
+  });
 }
+
