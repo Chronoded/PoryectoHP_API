88d22db56d09951b5fb8a0bec4e07d71d4b08de2
Chronoded <oli_olli@hotmail.com>
2022-12-01 08:41:39 -0700

    Agregue prueba Json y arregle detalles

--- potter_api_app/test/caracteristicas/repositorios/repositorio_json_test.dart
+++ potter_api_app/test/caracteristicas/repositorios/repositorio_json_test.dart
@@ -4,50 +4,71 @@ import 'package:potter_api_app/caracteristicas/repositorios/repositorio_json.dar
 
 void main() {
   group('Pruebas de Repositorio JSON', () {
-  test('cuando es offline y el archivo es datos_estudiantes.json devuelve un json', () async{
+    test(
+        'cuando es offline y el archivo es datos_estudiantes.json devuelve un json',
+        () async {
       RepositorioPruebaJson repoh = RepositorioPruebaJson();
-      var resultado = await repoh.obtenerDatos('offline', './lib/caracteristicas/data/datos_estudiantes.json');
+      var resultado = await repoh.obtenerDatos(
+          'offline', './lib/caracteristicas/data/datos_estudiantes.json');
       resultado.match((l) {
         assert(false);
       }, (r) {
-        expect(r[0]['name'], 'Harry Potter');
-      });    
-  });
-  test('cuando es online y url: https://hp-api.onrender.com/api/characters devuelve un json', () async{
-      RepositorioPruebaJson repoh = RepositorioPruebaJson();
-      var resultado = await repoh.obtenerDatos('online', 'https://hp-api.onrender.com/api/characters');
-      resultado.match((l) {
-        assert(false);
-      }, (r) {
-        expect(r[1]['name'], 'Hermione Granger');
+        expect(r, isA<List<dynamic>>());
       });
-  });
-  test('cuando es offline y ruta: "" arroja el problema JsonNoEncontrado', () async{
-    RepositorioPruebaJson repoy = RepositorioPruebaJson();
-    var resultado = await repoy.obtenerDatos('offline', '');
-    resultado.match((l) {
-      expect(l, isA<JsonNoEncontrado>());
-    }, (r) {
-      assert(false);
+    });
+    test(
+        'cuando es online y url: https://hp-api.onrender.com/api/characters devuelve un json',
+        () async {
+      RepositorioPruebaJson repoh = RepositorioPruebaJson();
+      var resultado = await repoh.obtenerDatos(
+          'online', 'https://hp-api.onrender.com/api/characters');
+      resultado.match((l) {
+        assert(false);
+      }, (r) {
+        expect(r, isA<List<dynamic>>());
+      });
+    });
+    test('cuando es offline y ruta: "" arroja el problema JsonNoEncontrado',
+        () async {
+      RepositorioPruebaJson repoy = RepositorioPruebaJson();
+      var resultado = await repoy.obtenerDatos('offline', '');
+      resultado.match((l) {
+        expect(l, isA<JsonNoEncontrado>());
+      }, (r) {
+        assert(false);
+      });
+    });
+    test('cuando es online y url: "" arroja el problema JsonNoEncontrado',
+        () async {
+      RepositorioPruebaJson repoy = RepositorioPruebaJson();
+      var resultado = await repoy.obtenerDatos('online', '');
+      resultado.match((l) {
+        expect(l, isA<JsonNoEncontrado>());
+      }, (r) {
+        assert(false);
+      });
+    });
+    test(
+        'cuando es offline y archivo: estonoesunjson.json arroja problema JsonNoEncontrado',
+        () async {
+      RepositorioPruebaJson repop = RepositorioPruebaJson();
+      var resultado = await repop.obtenerDatos(
+          'offline', './test/caracteristicas/data/datos_estonoesunjson.json');
+      resultado.match((l) {
+        expect(l, isA<JsonNoEncontrado>());
+      }, (r) {
+        assert(false);
+      });
     });
   });
-  test('cuando es online y url: "" arroja el problema JsonNoEncontrado', () async{
-    RepositorioPruebaJson repoy = RepositorioPruebaJson();
-    var resultado = await repoy.obtenerDatos('online', '');
-    resultado.match((l) {
-      expect(l, isA<JsonNoEncontrado>());
-    }, (r) {
-      assert(false);
-    }); 
-  });
-  test('cuando es offline y archivo: estonoesunjson.json arroja problema JsonNoEncontrado', () async{
-    RepositorioPruebaJson repop = RepositorioPruebaJson();
-    var resultado = await repop.obtenerDatos('offline', './test/caracteristicas/data/datos_estonoesunjson.json');
-    resultado.match((l) {
-      expect(l, isA<JsonNoEncontrado>());
-    }, (r) {
+
+    test('cuando es offline y archivo datos_incorrectos_estudiantes.json arroja JsonInexistente', () async{
+    RepositorioPruebaJson rpj = RepositorioPruebaJson();
+    var resultado = await rpj.obtenerDatos('offline', 'lib/caracteristicas/data/datos_incorrectos_estudiantes.json');
+    resultado.match((l){
+      expect(l, isA<JsonInexistente>());
+    }, (r){
       assert(false);
     });
-  });
   });
 }
