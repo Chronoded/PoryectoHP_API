ffd37105aeadf07809f51cd8b4c6405001304251
Chronoded <oli_olli@hotmail.com>
2022-12-01 09:19:45 -0700

    Cree repositorioMagias para hechizos online

--- potter_api_app/lib/caracteristicas/repositorios/repositorio_hechizos.dart
+++ potter_api_app/lib/caracteristicas/repositorios/repositorio_hechizos.dart
@@ -4,57 +4,67 @@ import 'package:potter_api_app/caracteristicas/dominio/nombre_personaje.dart';
 import 'package:potter_api_app/caracteristicas/repositorios/repositorio_json.dart';
 import '../dominio/problema.dart';
 
-String api ='https://hp-api.onrender.com/api/spells';
-String rutaJson='./lib/caracteristicas/data/datos_hechizos.json';
+String api = 'https://hp-api.onrender.com/api/spells';
+String rutaJson = './lib/caracteristicas/data/datos_hechizos.json';
 List<Hechizo> listaHechizos = [];
 
-abstract class RepositorioHechizo{
+abstract class RepositorioHechizo {
   late RepositorioPruebaJson constructor;
   RepositorioHechizo(this.constructor);
-  Future<Either<Problema,Hechizo>> obtenerHechizo(NombrePersonaje nombre);
+  Future<Either<Problema, Hechizo>> obtenerHechizo(NombrePersonaje nombre);
 }
-class RepositorioMagias extends RepositorioHechizo{
+
+class RepositorioMagias extends RepositorioHechizo {
   RepositorioMagias(super.constructor);
 
   @override
-  Future<Either<Problema, Hechizo>> obtenerHechizo(NombrePersonaje nombre) {
-    throw UnimplementedError();
+  Future<Either<Problema, Hechizo>> obtenerHechizo(NombrePersonaje nombre) async {
+    if (listaHechizos.isEmpty) {
+      var resultado = await constructor.obtenerDatos('online', api);
+      resultado.match((l) {
+        return Left(l);
+      }, (r) {
+        listaHechizos = obtenerListaHechizos(r);
+      });
+    }
+    for (var i = 0; i < listaHechizos.length; i++) {
+      if (listaHechizos[i].nombre == nombre.valor) {
+        return Right(listaHechizos[i]);
+      }
+    }
+    return Left(JsonInexistente());
   }
-
 }
-class RepositorioMagiasPruebas extends RepositorioHechizo{
+
+class RepositorioMagiasPruebas extends RepositorioHechizo {
   RepositorioMagiasPruebas(super.constructor);
 
   @override
-  Future<Either<Problema,Hechizo>> obtenerHechizo(NombrePersonaje nombre) async{
-    try {
-     var resultado = await constructor.obtenerDatos('offline', rutaJson);
-     resultado.match((l) {
-      return Left(l);
-      }, 
-      (r) {
-      listaHechizos = obtenerListaHechizos(r);
-      for (var i = 0; i < listaHechizos.length; i++) {
-        if (listaHechizos[i].nombre==nombre.valor) {
-          return Right(listaHechizos[i]);
-        }
-      } 
-      return Left(HechizoInexistente());
-      }); 
-    } catch (e) {
-      return Left(JsonNoEncontrado());
+  Future<Either<Problema, Hechizo>> obtenerHechizo(NombrePersonaje nombre) async {
+    if (listaHechizos.isEmpty) {
+      var resultado = await constructor.obtenerDatos('offline', rutaJson);
+      resultado.match((l) {
+        return Left(l);
+      }, (r) {
+        listaHechizos = obtenerListaHechizos(r);
+      });
     }
-    return Left(HechizoErroneo());
-  }
-
-  List<Hechizo> obtenerListaHechizos(List<dynamic> json) {
-    List<Hechizo> listaHechizos = [];
-    Hechizo a;
-    for (var i = 0; i < json.length; i++) {
-      a = Hechizo.constructor(nombre: json[i]['name'], descripcion: json[i]['description']);    
-      listaHechizos.add(a);
+    for (var i = 0; i < listaHechizos.length; i++) {
+      if (listaHechizos[i].nombre == nombre.valor) {
+        return Right(listaHechizos[i]);
+      }
     }
-    return listaHechizos;
+    return Left(HechizoInexistente());
   }
+}
 
+List<Hechizo> obtenerListaHechizos(List<dynamic> json) {
+  List<Hechizo> listaHechizos = [];
+  Hechizo a;
+  for (var i = 0; i < json.length; i++) {
+    a = Hechizo.constructor(
+        nombre: json[i]['name'], descripcion: json[i]['description']);
+    listaHechizos.add(a);
+  }
+  return listaHechizos;
 }
