81c637f039511887888535306d939e9ee8c646d0
Chronoded <oli_olli@hotmail.com>
2022-11-30 18:16:37 -0700

    cree repositorio de personaje, agregue dependencia

---
+++ potter_api_app/lib/caracteristicas/repositorios/repositorio_personaje.dart
@@ -0,0 +1,89 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:fpdart/fpdart.dart';
+import 'package:potter_api_app/caracteristicas/dominio/nombre_personaje.dart';
+import 'package:potter_api_app/caracteristicas/dominio/personajes.dart';
+import '../dominio/problema.dart';
+
+abstract class RepositorioPersonaje {
+  Either<Problema, Personaje> obtenerPersonaje(NombrePersonaje nombre);
+}
+
+class RepositorioPruebasPersonaje extends RepositorioPersonaje {
+  @override
+  Either<Problema, Personaje> obtenerPersonaje(NombrePersonaje nombre) {
+    Personaje p;
+    String jsonPersonaje = './lib/caracteristicas/data/datos_personaje.json';
+    List<dynamic> json;
+    try {
+      json = jsonDecode((File(jsonPersonaje).readAsStringSync()));
+    } catch (e) {
+      return Left(JsonNoEncontrado());
+    }
+    String nombrejson;
+    List<dynamic> nombresAlt;
+    String especie;
+    String escuela;
+    String fechaNac;
+    int anioNac;
+    bool mago;
+    String ancestro;
+    String colorOjos;
+    String colorCabello;
+    Map varita;
+    String patronus;
+    bool estudianteHowarts;
+    bool varitaHowarts;
+    String actor;
+    List<dynamic> actoresAlt;
+    bool vive;
+    String imagen;
+    for (var i = 0; i < json.length; i++) {
+      try {
+        if (json[i]["name"] == nombre.nombre) {
+          nombrejson = json[i]['name'];
+          nombresAlt = json[i]['alternate_names'];
+          especie = json[i]['species'];
+          escuela = json[i]['house'];
+          fechaNac = json[i]['dateOfBirth'];
+          anioNac = json[i]['yearOfBirth'];
+          mago = json[i]['wizard'];
+          ancestro = json[i]['ancestry'];
+          colorOjos = json[i]['eyeColour'];
+          colorCabello = json[i]['hairColour'];
+          varita = json[i]['wand'];
+          patronus = json[i]['patronus'];
+          estudianteHowarts = json[i]['hogwartsStudent'];
+          varitaHowarts = json[i]['hogwartsStaff'];
+          actor = json[i]['actor'];
+          actoresAlt = json[i]['alternate_actors'];
+          vive = json[i]['alive'];
+          imagen = json[i]['image'];
+          p = Personaje.constructor(
+              nombre: nombrejson,
+              actor: actor,
+              actoresAlt: actoresAlt,
+              ancestro: ancestro,
+              anioNac: anioNac,
+              colorCabello: colorCabello,
+              colorOjos: colorOjos,
+              escuela: escuela,
+              especie: especie,
+              estudianteHowarts: estudianteHowarts,
+              fechaNac: fechaNac,
+              mago: mago,
+              nombresAlt: nombresAlt,
+              imagen: imagen,
+              patronus: patronus,
+              varita: varita,
+              varitaHowarts: varitaHowarts,
+              vive: vive);
+          return Right(p);
+        }
+      } catch (e) {
+        return Left(JsonInexistente());
+      }
+    }
+    return Left(PersonajeNoEncontrado());
+  }
+}
