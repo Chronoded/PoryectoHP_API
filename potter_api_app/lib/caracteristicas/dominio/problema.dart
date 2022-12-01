f54236ba16724808ad662912b1b1d3577e2532dd
Chronoded <oli_olli@hotmail.com>
2022-11-30 17:55:12 -0700

    Agregue clases a problema.dart

--- potter_api_app/lib/caracteristicas/dominio/problema.dart
+++ potter_api_app/lib/caracteristicas/dominio/problema.dart
@@ -3,3 +3,9 @@ class Problema {}
 class PersonajeInexistente extends Problema {}
 
 class NombreInexistente extends Problema {}
+
+class PersonajeNoEncontrado extends Problema{}
+
+class JsonNoEncontrado extends Problema {}
+ 
+class JsonInexistente extends Problema {}
